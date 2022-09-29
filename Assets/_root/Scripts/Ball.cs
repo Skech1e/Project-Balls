2ea4c1235a1eb07c00e568f9be1e370574893279
skech1e <zeo.awper@gmail.com>
2022-09-29 18:03:15 +0530

    tried trajectory

--- Assets/_root/Scripts/Ball.cs
+++ Assets/_root/Scripts/Ball.cs
@@ -8,8 +8,14 @@ public class Ball : MonoBehaviour
     LineRenderer trajectory;
 
     [SerializeField] Vector3 currentPosition, targetPosition;
-    [SerializeField] float forceMultiplier, minForce, maxForce;
-    [SerializeField] float Angle, ballheight, trajectoryPoint;
+    [SerializeField] float forceMultiplier, minForce, maxForce, throwForce;
+    [SerializeField] float Angle, ballheight, trajectoryPoint, flightTime, mass, interval;
+
+    [SerializeField] int maxSteps;
+
+    Vector2 tracjectoryPath;
+
+    [SerializeField] List<Vector2> points = new();
 
     [Header("Debug")]
     [SerializeField]
@@ -22,18 +28,22 @@ public class Ball : MonoBehaviour
         currentPosition = transform.position;
         ballheight = currentPosition.y;
         trajectory = GetComponent<LineRenderer>();
+        trajectory.positionCount = 20;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxSteps = (int)(flightTime/interval);        
     }
 
     // Update is called once per frame
     void Update()
     {
-        DrawTrajectory();
+        throwForce = throwForce/mass * Time.fixedDeltaTime;
+        currentPosition = transform.position;
+        //DrawTrajectory();
+        //Test();
     }
 
     void DrawTrajectory()
@@ -57,5 +67,17 @@ public class Ball : MonoBehaviour
 
         trajectory.positionCount = curvePoints.Count;
         trajectory.SetPositions(curvePoints.ToArray());
+    }
+
+    void Test()
+    {
+        for(int i = 0; i < maxSteps; i++)
+        {
+            tracjectoryPath = currentPosition + transform.up + i * interval * throwForce * transform.up;
+            tracjectoryPath.y += Physics.gravity.y / 2 * Mathf.Pow(i * interval, 2);
+            points.Add(tracjectoryPath);
+
+            trajectory.SetPosition(i, points[i]);
+        }
     }
 }
---
+++ Assets/_root/Scripts/Trajectory.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{
+    [SerializeField] LineRenderer line;
+    [SerializeField] int lineSegment;
+    [SerializeField] Vector3 velocity;
+    [SerializeField] float gravity;
+
+
+    private void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+        lineSegment = 20;
+        gravity = Mathf.Abs(Physics.gravity.y);
+    }
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        StartCoroutine(RenderLine());
+    }
+
+    IEnumerator RenderLine()
+    {
+        line.positionCount = lineSegment + 1;
+        line.SetPositions(CalculateLineArray());
+        yield return null;
+    }
+
+    private Vector3[] CalculateLineArray()
+    {
+        Vector3[] lineArray = new Vector3[lineSegment + 1];
+
+        for(int i = 0; i < lineSegment; i++)
+        {
+            var t = i / (float)lineArray.Length;
+            lineArray[i] = CalculateLinePoints(t);
+        }
+        return lineArray;
+    }
+
+    private Vector3 CalculateLinePoints(float t)
+    {
+        float x = velocity.x * t;
+        float y = (velocity.y * t) - (gravity * Mathf.Pow(t, 2) / 2);
+        return new Vector3(x + transform.position.x, y + transform.position.y, x + transform.position.z);
+    }
+}
