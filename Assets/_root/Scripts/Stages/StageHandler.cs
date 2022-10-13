feda04c50617049961a05a78b7474e43c3623b9d
skech1e <zeo.awper@gmail.com>
2022-10-13 17:41:53 +0530

    stack implementation underway

--- Assets/_root/Scripts/Stages/StageHandler.cs
+++ Assets/_root/Scripts/Stages/StageHandler.cs
@@ -6,16 +6,24 @@ public class StageHandler : MonoBehaviour
 {
 
     [SerializeField] List<Transform> level = new();
-    [SerializeField] Scored basket;
+    [SerializeField] List<Scored> basket = new();
     [SerializeField] PositionsArray pa;
 
     [Header("Debug")]
     [SerializeField] ArrayViewer av;
     public bool test;
     float offset = 0.8f;
-    
+    public int count;
+    enum LevelType { still, moving }
+
+    LevelType leveltype = new LevelType();
+
+    [SerializeField] Stack <int> istack, jstack = new();
+
     private void Awake()
     {
+        istack.Count = 4;
+        jstack.Count = 4;
         level.Capacity = 10;
         //level[0] = pa.positionArray[0];
 
@@ -23,16 +31,34 @@ public class StageHandler : MonoBehaviour
 
     private void OnValidate()
     {
+        count = 0;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 5; j++)
             {
                 if (av.columns[i].rows[j] == true)
                 {
-                    basket.transform.parent.position = pa.target[i, j].position;
+                    istack.Push(i);
+                    jstack.Push(j);
+                    count++;
+                    if(count > 3)
+                    {
+
+                    }
+                    basket[count].transform.parent.position = pa.target[i, j].position;
                     //basket.transform.parent.position += Vector3.back * offset;
                 }
             }
+        }
+
+
+        switch (leveltype)
+        {
+            case LevelType.still:
+
+                break;
+            case LevelType.moving:
+                break;
         }
     }
 
@@ -45,6 +71,6 @@ public class StageHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
